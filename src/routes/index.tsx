faffddebb3b3a34dd3d64e34b30237c0ff445b9a
leonardopereiradosantos3 <158337045+leonardopereiradosantos3@users.noreply.github.com>
2024-02-01 06:56:16 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <Title>Painéis Solares Fotovoltaicos</Title>
-      <h1>Os painéis solares fotovoltaicos são a espinha dorsal da energia solar. Compostos por células de silício, esses painéis convertem diretamente a luz solar em eletricidade. Esta tecnologia tem evoluído, tornando-se uma fonte acessível e eficiente de energia.</h1>
+      <h3>Os painéis solares fotovoltaicos são a espinha dorsal da energia solar. Compostos por células de silício, esses painéis convertem diretamente a luz solar em eletricidade. Esta tecnologia tem evoluído, tornando-se uma fonte acessível e eficiente de energia.</h3>
       <Counter />
 
 
