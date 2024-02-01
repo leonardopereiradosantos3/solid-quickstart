1f7f386cdb3e815c0bb6f2ee3af498b8f3d2cf68
leonardopereiradosantos3 <158337045+leonardopereiradosantos3@users.noreply.github.com>
2024-02-01 06:48:29 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,11 +4,10 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>teste</h1>
+      <Title>Painéis Solares Fotovoltaicos</Title>
+      <h1>Os painéis solares fotovoltaicos são a espinha dorsal da energia solar. Compostos por células de silício, esses painéis convertem diretamente a luz solar em eletricidade. Esta tecnologia tem evoluído, tornando-se uma fonte acessível e eficiente de energia.</h1>
       <Counter />
 
- <embed type="text/html" src="https://energiasolarpratodos.com.br/" width="500" height="200">
 
       <p>
         Visit{" "}
