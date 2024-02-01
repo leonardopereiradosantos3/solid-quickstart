7d104dc95028f617ce7ea55ef4a647ba7e944fdf
leonardopereiradosantos3 <158337045+leonardopereiradosantos3@users.noreply.github.com>
2024-02-01 07:29:30 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,9 +5,8 @@ export default function Home() {
   return (
     <main>
       <Title>Painéis Solares Fotovoltaicos</Title>
-      <h3>Os painéis solares fotovoltaicos são a espinha dorsal da energia solar. Compostos por células de silício, esses painéis convertem diretamente a luz solar em eletricidade. Esta tecnologia tem evoluído, tornando-se uma fonte acessível e eficiente de energia.</h3><br>
-      Seção 1: Introdução à Energia Solar
-A energia solar é uma fonte de energia renovável derivada da luz do sol. É uma solução sustentável e limpa para atender às necessidades de eletricidade e aquecimento, contribuindo para a redução das emissões de gases de efeito estufa e promovendo a sustentabilidade ambiental.</br>
+      <h3>Os painéis solares fotovoltaicos são a espinha dorsal da energia solar. Compostos por células de silício, esses painéis convertem diretamente a luz solar em eletricidade. Esta tecnologia tem evoluído, tornando-se uma fonte acessível e eficiente de energia.</h3>
+      <meta name="google-site-verification" content="e8k73gX96SbCrCmKe2yKaq1j9UCx6sdwc2uu97OHjss" />
       <Counter />
 
 
